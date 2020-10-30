0956a13ae9d8a9201a061dc05a34533b6bf7c4db
Zilfa Cyamani <zilcyam@gmail.com>
2020-10-30 22:41:37 +0200

    change quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -12,7 +12,7 @@ export class QuoteComponent implements OnInit {
     new Quote(1, 'When you learn, teach, when you get, give.','Maya Angelou','Zilfa',new Date(2020,3,14),5,2),
     new Quote(2,'Live as if you were to die tomorrow. Learn as if you were to live forever.','Mahatma Gandhi', 'Diane',new Date(1971,4,11),4,8),
     new Quote(3,'Nothing can dim the light shines which shines within.','Maya Angelou', 'Brendah',new Date(1965,9,6),5,4),
-    new Quote(5,'When people show you who they are, beleive them .','Maya Angelou', 'Tharcissie',new Date(1988,9,5),8,4),
+    new Quote(5,'Do one thing every day that scares you.','Eleanor Roosevelt', 'Tharcissie',new Date(1988,9,5),8,4),
   ];
 
   toggleDetails(index){
