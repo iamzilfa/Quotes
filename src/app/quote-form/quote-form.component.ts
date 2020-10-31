340c43e99c4d893e14d4975cc7d86da320a55144
Zilfa Cyamani <zilcyam@gmail.com>
2020-10-31 20:59:48 +0200

    edit form

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-form',
-  templateUrl: './quote-form.component.html',
+  templateUrl:'./quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
 
-  newQuote = new Quote(0,"","","",new Date(),0,0);
+  newQuote = new Quote(0, "", "", "", new Date(), 0, 0);
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  submitQuote() {
+    this.addQuote.emit(this.newQuote);
+  }
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,10 +9,10 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    new Quote(1, 'When you learn, teach, when you get, give.','Maya Angelou','Zilfa',new Date(2020,3,14),5,2),
-    new Quote(2,'Live as if you were to die tomorrow. Learn as if you were to live forever.','Mahatma Gandhi', 'Diane',new Date(1971,4,11),4,8),
-    // new Quote(3,'Nothing can dim the light shines which shines within.','Maya Angelou', 'Brendah',new Date(1965,9,6),5,4),
-    // new Quote(5,'Do one thing every day that scares you.','Eleanor Roosevelt', 'Tharcissie',new Date(1988,9,5),8,4),
+    new Quote(1, 'When you learn, teach, when you get, give.','Maya Angelou','Admin',new Date(2020,3,14),0,0),
+    new Quote(2,'Live as if you were to die tomorrow. Learn as if you were to live forever.','Mahatma Gandhi', 'Admin',new Date(1971,4,11),0,0),
+    new Quote(3,'Nothing can dim the light shines which shines within.','Maya Angelou', 'Admin',new Date(1965,9,6),0,0),
+    new Quote(5,'Do one thing every day that scares you.','Eleanor Roosevelt', 'Admin',new Date(1988,9,5),0,0),
   ];
 
   toggleDetails(index){
@@ -27,6 +27,13 @@ export class QuoteComponent implements OnInit {
         this.quotes.splice(index,1)
       }
     }
+  }
+
+  addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
   }
 
   constructor() { }
